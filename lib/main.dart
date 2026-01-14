fbdd8e344cc2cb5c613b5ea884fb0cac726f908d
Faith Kariuki <tcwamboi@gmail.com>
2026-01-14 10:42:28 +0300

    Add padding and margin using a Container widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,21 +15,29 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(
-        // child: Image.asset('assets/smiley.png'),
-        // child: ElevatedButton.icon(
-        //   onPressed: () {},
-        //   icon: Icon(Icons.mail),
-        //   label: Text("mail me"),
-        //   style: ElevatedButton.styleFrom(backgroundColor: Colors.amber),
-        // ),
-        child: IconButton(
-          onPressed: () {
-            print("You clicked me!");
-          },
-          icon: Icon(Icons.alternate_email),
-          color: Colors.amber,
-        ),
+      // body: Center(
+      //   // child: Image.asset('assets/smiley.png'),
+      //   // child: ElevatedButton.icon(
+      //   //   onPressed: () {},
+      //   //   icon: Icon(Icons.mail),
+      //   //   label: Text("mail me"),
+      //   //   style: ElevatedButton.styleFrom(backgroundColor: Colors.amber),
+      //   // ),
+      //   child: IconButton(
+      //     onPressed: () {
+      //       print("You clicked me!");
+      //     },
+      //     icon: Icon(Icons.alternate_email),
+      //     color: Colors.amber,
+      //   ),
+      // ),
+      body: Container(
+        // padding: EdgeInsets.all(20.0),
+        // padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+        padding: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
+        margin: EdgeInsets.all(30.0),
+        color: Colors.grey[400],
+        child: Text("hello"),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
