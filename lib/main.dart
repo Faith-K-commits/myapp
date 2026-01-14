86800a0b955bdf4529709f51f870054940d392cd
Faith Kariuki <tcwamboi@gmail.com>
2026-01-14 10:44:49 +0300

    Add padding without Container widget

--- lib/main.dart
+++ lib/main.dart
@@ -31,14 +31,15 @@ class Home extends StatelessWidget {
       //     color: Colors.amber,
       //   ),
       // ),
-      body: Container(
-        // padding: EdgeInsets.all(20.0),
-        // padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
-        padding: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
-        margin: EdgeInsets.all(30.0),
-        color: Colors.grey[400],
-        child: Text("hello"),
-      ),
+      // body: Container(
+      //   // padding: EdgeInsets.all(20.0),
+      //   // padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+      //   padding: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
+      //   margin: EdgeInsets.all(30.0),
+      //   color: Colors.grey[400],
+      //   child: Text("hello"),
+      // ),
+      body: Padding(padding: EdgeInsets.all(90.0), child: Text('hello')),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: Colors.red[600],
